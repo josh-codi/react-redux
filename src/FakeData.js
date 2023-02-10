58eb80db7976164a0730f9bdfa703a65bece37ee
josh-codi <odamejoshua37@gmail.com>
2023-02-10 08:56:12 +0000

    setup redux and reduxtoolkit store

---
+++ src/FakeData.js
@@ -0,0 +1,17 @@
+export const UsersData = [
+    {
+        id: 1,
+        name: 'Josh Joy',
+        username: 'joshjoy'
+    },
+    {
+        id: 2,
+        name: 'Debby Love',
+        username: 'debbylove'
+    },
+    {
+        id: 3,
+        name: 'Opana Emmanuel',
+        username: 'opanaemma'
+    }
+]
